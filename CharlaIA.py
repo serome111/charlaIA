76df51c609919c8989f717bced9879504df81291
serome111 <32885503+serome111@users.noreply.github.com>
2017-12-06 10:41:03 -0500

    Update CharlaIA.py

--- CharlaIA.py
+++ CharlaIA.py
@@ -1,41 +1,52 @@
 #!/usr/bin/python
-
 class datos:
 	def __init__(self):
+		self.a="string"
 		self.primeras50=[] #aqui se guardan todos los datos de entrada
-		self.datosPreguta=[] #aqui se deben guardar los datos que hacen preguntas
-		self.datosRespuesta=[] #aqui se guardan todas las respuestas
-		self.datosPR={}	#aqui se guardan las preguntas con sus respuestas
+		self.datosRespuesta=[]
+		self.datosPR={}
 		self.BuscarMayor=[] #aqui se agragan los numeros de repeticiones
 		self.i=0
 		self.pregunta=""
+		self.c=0
 	def ensenar(self):
-		for contador in range (0,6): #que se repita todo en un rango de ("1" a lo que ponga) entrenador
-			self.primeras50.append(raw_input("Lord "))
-		
+		for contador in range (0,6):
+			a = raw_input("Que ordenas mi Lord:")
+			if(a in self.datosPR): # si la palabra  ya tiene respuesta
+				print self.datosPR[a]
+				while (a in self.primeras50):
+					self.primeras50.remove(a)
+
+			else:
+				self.primeras50.append(a)
+				while self.i<contador:
+					break
 		for contador in self.primeras50:
 			self.i += 1
-			count = self.primeras50.count(self.primeras50[self.i-1]) #contador de datos mas repetidos
+			count = self.primeras50.count(self.primeras50[self.i-1])
 			#print count
 			#print "el dato ",self.primeras50[self.i-1],"se repite ",count, "veces "
 			self.BuscarMayor.append(count)
+			#print(self.primeras50[])
 
-		posicion = self.BuscarMayor.index(max(self.BuscarMayor)) #posicion del dato mas repetido
-		self.pregunta=self.primeras50[posicion]
-			
+		posicion = self.BuscarMayor.index(max(self.BuscarMayor))
+		n=self.pregunta=self.primeras50[posicion]
+		#print (self.primeras50())
+			#drop propiedades de las listas
+			#npl procesamiento de lenguaje natutral
+			#regresion lineal simple 3euristica
 		print (contador)
 		print (self.BuscarMayor)
 		self.recordar()
 	def recordar(self):
-		print "pregunta aprendida -->",self.pregunta #datosRespuesta[0] #lo que aprende a decir
-		res = raw_input("Lord:") #Pues aqui debe ir se supone el dato de respuesta a la pregunta
+		print "aprendio a decir -->",self.pregunta # datosRespuesta[0]
+		res = raw_input("Lord:")
 		self.datosPR[self.pregunta]=res
 		print(self.datosPR)
-		self.primeras50=[]
+		self.primras50=[]
 		self.i=0
 		self.BuscarMayor=[]
 		self.ensenar()
-		
 
 per=datos()
 per.ensenar()
