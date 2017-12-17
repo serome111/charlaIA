a5e4a8e8f22030d9c9e84f05b32661dc023ae554
serome111 <32885503+serome111@users.noreply.github.com>
2017-12-16 19:43:20 -0500

    6
    con algunas mejoras mas

--- CharlaIA.py
+++ CharlaIA.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python
+import sys
+
 class datos:
 	def __init__(self):
 		self.a="string"
 		self.primeras50=[] # aqui se guardan todos los datos de entrada
-		self.datosRespuesta=[]
+		self.datosRespuesta=[] # respuestas que se le ensenaron
 		self.datosPR={}
 		self.BuscarMayor=[] # aqui se agragan los numeros de repeticiones
 		self.i=0
 		self.pregunta=""
 		self.c=0
+		self.aprende=["aprende esto","aprende esto","se dice a si"] # en uso
+		self.nuevaPOT=[] # nueva palabra o texto
 	def ensenar(self):
 		for contador in range (0,6):
 			self.a = raw_input("Lord:")
+			self.sistema()
+			self.aprender()
 			if(self.a in self.datosPR): # si la palabra  ya tiene respuesta
 				print self.datosPR[self.a]
-				# while (self.a in self.primeras50):
-					# self.primeras50.remove(self.a)
 
 			else:
 				self.primeras50.append(self.a)
@@ -36,23 +40,31 @@ class datos:
 		self.recordar()
 	def recordar(self):
 
-		print self.primeras50
-		print ("palabra guardada en self.a",self.a)
+		# print self.primeras50
+		# print ("palabra guardada en self.a",self.a)
 		print "aprendio a decir -->",self.pregunta # datosRespuesta[0]
 		res = raw_input("Lord:")
 		self.datosPR[self.pregunta]=res
-		# while (self.pregunta in self.primeras50):
-			 # self.primeras50.remove(self.a)
-
 		print(self.datosPR)
 		self.primras50=[]
 		self.i=0
 		self.BuscarMayor=[]
+		print ("Este es self.datosRespuesta" , self.datosRespuesta)
 		while (self.pregunta in self.primeras50):
 			self.primeras50.remove(self.pregunta)
 		self.ensenar()
-		# while (a in sel.primeras50):
-			# self.primeras50.remove(a)
+	def aprender(self):
+
+		if (self.a == self.aprende[0] or self.a == self.aprende[1] or self.a == self.aprende[2]):
+			print "Genial necesito aprender dime"
+			self.nuevaPOT = raw_input("Cual es la pregunta: ")
+			self.datosRespuesta = raw_input("cual es la respuesta: ")
+			self.datosPR[self.nuevaPOT]=self.datosRespuesta # aqui se guarda la pregunta y la respuesta
+			self.ensenar()
+	def sistema(self):
+
+		if (self.a == "salir"):
+			exit()
 
 per=datos()
 per.ensenar()
